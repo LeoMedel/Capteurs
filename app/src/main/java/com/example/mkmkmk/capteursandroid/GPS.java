877d7ecd6fba70778f97c3c1064dc641ba286bd7
LeoMedel <leonel.ilhuicatzi07@gmail.com>
2018-06-20 17:42:22 +0200

    add gps distance function

--- app/src/main/java/com/example/mkmkmk/capteursandroid/GPS.java
+++ app/src/main/java/com/example/mkmkmk/capteursandroid/GPS.java
@@ -1,64 +1,97 @@
 package com.example.mkmkmk.capteursandroid;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutionException;
+
 public class GPS extends AppCompatActivity {
 
+
+    int count =0;
     double latitude = 0.0;
     double longitude = 0.0;
+
     double lastLatitude = 0.0;
     double lastLongitude = 0.0;
-    float distance = 0.0f;
 
     Location location;
-    boolean isGPSEnabled = false;
-    boolean isNetworkEnabled = false;
-    boolean isGPSTrackingEnabled = false;
-    private String provider_info;
 
     // Declaring a Location Manager
     protected LocationManager locationManager;
 
-
     TextView txtLatitude;
     TextView txtLongitude;
+    TextView txtLastLat;
+    TextView txtLastLon;
     TextView lastUbication;
+    Button btnDistance;
 
     // The minimum distance to change updates in meters
-    private static final long DISTANCE_METRES = 10; // 10 meters
+    private static final long DISTANCE_METRES = 3; // 3 meters
 
     //Mis a jour apres un minute
     // The minimum time between updates in milliseconds
-    private static final long TIME_UPDATE = 15000; // 15 secondes
+    private static final long TIME_UPDATE = 5000; // 5 seconds
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
 
-
         txtLatitude = (TextView) findViewById(R.id.txt_Latitude);
         txtLongitude = (TextView) findViewById(R.id.txt_Longitude);
+        txtLastLat = (TextView) findViewById(R.id.txtLastLatitude);
+        txtLastLon = (TextView) findViewById(R.id.txtLastLongitude);
         lastUbication = (TextView) findViewById(R.id.txtDistance);
+        btnDistance = (Button) findViewById(R.id.btnDistance);
 
         txtLatitude.setText("Bonjour");
         txtLongitude.setText("le Monde ...");
 
+        //startUbication();
         monUbication();
+
     }
 
+    public void startUbication()
+    {
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //new AttemptUpdate().execute();
+                        Log.i("Repeat", "This'll run 5 seconds later");
+                        count++;
+                        monUbication();
+                    }
+                });
+            }
+        }, 0, 5000);
+    }
 
     private void monUbication() {
 
@@ -66,26 +99,37 @@ public class GPS extends AppCompatActivity {
             return;
         }
 
+        LocationListener locationListener = new LocationListener() {
+            //S'il y a un change d'ubication il va faire un mis a jour
+            @Override
+            public void onLocationChanged(Location location) {
+                updateUbication(location);
+
+            }
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-        if (isGPSEnabled) {
-            provider_info = LocationManager.GPS_PROVIDER;
-        } else if (isNetworkEnabled) {
-            provider_info = LocationManager.NETWORK_PROVIDER;
-        }
-
-        if (!provider_info.isEmpty()) {
-            locationManager.requestLocationUpdates(provider_info, TIME_UPDATE, DISTANCE_METRES, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, TIME_UPDATE, DISTANCE_METRES, locationListener);
 
             if (locationManager != null) {
-                location = locationManager.getLastKnownLocation(provider_info);
+
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                 updateUbication(location);
-                Toast.makeText(this, "Update Ubication", Toast.LENGTH_SHORT).show();
-                lastUbication();
+
             }
-        }
     }
 
     private void updateUbication(Location location) {
@@ -95,72 +139,43 @@ public class GPS extends AppCompatActivity {
 
             txtLatitude.setText("Latitude =>  " + latitude);
             txtLongitude.setText("Longitude =>  " + longitude);
-            Log.i("GPS Longitude", "Longitude => " + longitude);
-            Log.i("GPS Longitude", "Latitude => " + latitude);
 
+            if (lastLatitude == 0.0 || lastLongitude == 0.0)
+            {
+                lastLatitude = latitude;
+                lastLongitude = longitude;
+
+                txtLastLat.setText("Derniere latitude : "+lastLatitude);
+                txtLastLon.setText("Derniere longitude : "+lastLongitude);
+            }
+
+            Toast.makeText(this, "Update Ubication", Toast.LENGTH_SHORT).show();
         }
     }
 
-    LocationListener locationListener = new LocationListener() {
-        //S'il y a un change d'ubication il va faire un mis a jour
-        @Override
-        public void onLocationChanged(Location location) {
-            lastUbication();
-            updateUbication(location);
+    public void setDistance(View view)
+    {
+        //variables de type Location avec les coordonnes correspondant
+        Location l = new Location("Location 1");
+        l.setLatitude(latitude);
+        l.setLongitude(longitude);
 
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
+        Location l2 = new Location("Location 2");
+        l2.setLatitude(lastLatitude);
+        l2.setLongitude(lastLongitude);
 
-            txtLatitude.setText("Latitude =>  " + latitude);
-            txtLongitude.setText("Longitude =>  " + longitude);
+        //on obtiens la distance
+        double distanceM = l.distanceTo(l2);
+        double distanceK = distanceM/1000;
 
+        StringBuilder message = new StringBuilder();
+        message.append(String.format("%.2f", distanceM)+"m");
+        message.append("\r\n");
+        message.append(String.format("%.2f", distanceK)+"Km");
 
-        }
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-
-        }
-    };
-
-    private void lastUbication() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-        lastLatitude = location.getAltitude();
-        lastLongitude = location.getLongitude();
-
-
-        Location locationA = new Location("punto A");
-
-        locationA.setLatitude(lastLatitude);
-        locationA.setLongitude(lastLongitude);
-
-        Location locationB = new Location("punto B");
-
-        locationB.setLatitude(latitude);
-        locationB.setLongitude(longitude);
-
-        distance += locationA.distanceTo(locationB);
-
-        lastUbication.setText("Distance (Mètres) parcouru : "+distance);
+        lastUbication.setText(message.toString());
     }
+
 }
+
+
